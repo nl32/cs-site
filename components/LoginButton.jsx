3174c1a1daef0a2578527a9706b47a88975177c0
Ethan Bickel <notethanbickel@gmail.com>
2022-05-09 14:07:49 -0500

    Added login button

---
+++ components/LoginButton.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import useUser from "../lib/useUser";
+import { useEffect } from "react";
+import { useState } from "react";
+import styles from "../styles/LoginButton.module.css";
+export default function LoginButton() {
+  const { user, mutateUser } = useUser();
+  const router = useRouter();
+  const [text, setText] = useState("");
+  useEffect(() => {
+    setText(user && user.username ? "logout" : "login");
+    console.log("user changed");
+  }, [user]);
+  return (
+    <button
+      className={styles.button}
+      onClick={async (e) => {
+        e.preventDefault();
+        if (user.username) {
+          mutateUser(await axios.post("/api/auth/logout"), false);
+          router.push("/logout");
+        } else {
+          router.push("/login");
+        }
+      }}
+    >
+      {text}
+    </button>
+  );
+}
