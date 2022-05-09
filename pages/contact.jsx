2d5e00a3cf570ab575126643a1a0811af361d1b5
Ethan Bickel <notethanbickel@gmail.com>
2022-05-09 14:22:58 -0500

    added info pages

---
+++ pages/contact.jsx
@@ -0,0 +1,10 @@
+export default function contact(props) {
+  return (
+    <div>
+      <p>
+        This website was constructed by Ethan Bickel, a cs 3 student at mckinney
+        high school as class project. To reach out you can contact him at:{" "}
+      </p>
+    </div>
+  );
+}
