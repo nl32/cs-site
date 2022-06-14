93e9373ecc82f2ec4114a1880ee87256efb225c9
Ethan Bickel <notethanbickel@gmail.com>
2022-06-13 20:09:29 -0500

    added global css

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,12 +2,13 @@ import { SWRConfig } from "swr";
 import Layout from "../components/Layout";
 import { fetcher } from "../lib/swr";
 import Head from "next/head";
+import styles from "../styles/global.module.css";
 export default function app({ Component, pageProps }) {
   return (
-    <div>
+    <div className={styles.app}>
       <Head>
         <title>Mhs CS</title>
-        <link rel="icon" href="/favicon.ico"/>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <SWRConfig
         value={{ fetcher: fetcher, onError: (err) => console.log(err) }}
