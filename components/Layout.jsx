303fb811b181732b594a04483b2181fa788670f3
Ethan Bickel <notethanbickel@gmail.com>
2022-06-13 22:49:43 -0500

    div -> span

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,12 +3,12 @@ import Footer from "./Footer";
 import styles from "../styles/Layout.module.css";
 export default function Layout({ children }) {
   return (
-    <span className={styles.pageContainer}>
-      <span className={styles.contentWrap}>
+    <div className={styles.pageContainer}>
+      <div className={styles.contentWrap}>
         <NavBar />
         <>{children}</>
-      </span>
+      </div>
       <Footer />
-    </span>
+    </div>
   );
 }
