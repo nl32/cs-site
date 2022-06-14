addfa01c075f73062b7b89bdbfe87b416ebadf50
Ethan Bickel <notethanbickel@gmail.com>
2022-06-13 20:28:07 -0500

    remove unused import

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,4 @@
 import NavBar from "./NavBar";
-import ToastManager from "./toast/ToastManager";
 export default function Layout({ children }) {
   const testList = [{ title: "error", description: "there is an error" }];
   return (
