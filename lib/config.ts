db4f777f03e616b6d847d44647b6ee60c4a8e5dd
Ethan Bickel <notethanbickel@gmail.com>
2022-06-26 13:50:42 -0500

    type safety hack

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,6 @@
 export const ironOptions = {
   cookieName: "mhscs_cookie",
-  password: process.env.AUTH_PASSWORD,
+  password: process.env.AUTH_PASSWORD || "",
   cookieOptions: {
     secure: process.env.NODE_ENV === "production",
   },
