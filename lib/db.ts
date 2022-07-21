f22e1417738c8e7fdc9ad02d35c56de92ba2e1f1
Ethan Bickel <notethanbickel@gmail.com>
2022-07-21 04:33:51 -0500

    eslint :(

--- lib/db.ts
+++ lib/db.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 declare global {
   // eslint-disable-next-line no-unused-vars
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
