ab5d6a77508a3a9c36f356a73eef1009604bcb4a
Ethan Bickel <notethanbickel@gmail.com>
2022-07-23 14:35:00 -0500

    pls fix the issue i swear to god

--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -1,19 +1,18 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import Profile from "../../components/Profile";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
-import { useEffect } from "react";
 import { requireAuth } from "../../lib/requireAuth";
-import { getUser } from "../../lib/user";
 import { trpc } from "../../utils/trpc";
 export const getServerSideProps = requireAuth(async (ctx) => {
   return { props: {} };
 });
 export default function profile() {
-  const router = useRouter();
   const { data } = useSession();
   const userQuery = trpc.useQuery(["user.get", { id: data?.id as string }]);
+  if(userQuery.isLoading){
+    return <>Loading.</>
+  }
   return (
     <div>
       <Profile user={userQuery.data} />
