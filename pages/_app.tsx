81db3e14a819733b7694935b0a03620bc6b19fbe
Ethan Bickel <notethanbickel@gmail.com>
2022-07-23 15:12:45 -0500

    fix deployment error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -38,7 +38,7 @@ const app = ({ Component, pageProps }: AppProps) => {
 
 export default withTRPC<AppRouter>({
   config({ ctx }) {
-    const url = process.env.VERCEL_URL
+    const url = process.env.VERCEL
       ? `https://${process.env.VERCEL_URL}/api/trpc`
       : "http://localhost:3000/api/trpc";
     return {
