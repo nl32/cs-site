2ef27097f9e0705512a8ab4b519deecaa2ff7954
Ethan Bickel <notethanbickel@gmail.com>
2022-07-23 01:56:35 -0500

    auth utils

---
+++ lib/requireAuth.ts
@@ -0,0 +1,23 @@
+import type { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { unstable_getServerSession } from "next-auth";
+
+import { nextAuthOptions } from "./auth";
+
+export const requireAuth =
+  (func: GetServerSideProps) => async (ctx: GetServerSidePropsContext) => {
+    const session = await unstable_getServerSession(
+      ctx.req,
+      ctx.res,
+      nextAuthOptions
+    );
+
+    if (!session) {
+      return {
+        redirect: {
+          destination: "/user/login",
+          permanent: false
+        }
+      };
+    }
+    return await func(ctx);
+  };
